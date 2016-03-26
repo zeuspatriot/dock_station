7a5835deb046ee06dba7db7fb9edf1efc59d47f3
Dmitriy Horbachev <zeuspatriot@live.com>
2016-03-26 23:51:23 +0100

    Youtrack integration established Workitems are created and updated properly.

--- client/workitems.js
+++ client/workitems.js
@@ -1,3 +1,36 @@
+function summEstimate(test){
+    var lastEstimate = test.estimate[test.estimate.length-1].items;
+    var result = {};
+    lastEstimate.forEach(function(item){
+        if(result[item.group]){
+            result[item.group].name += ", " + item.name;
+            result[item.group].devEst += item.devEst;
+            result[item.group].qcEst += item.qcEst;
+        }
+        else{
+            result[item.group] = {
+                name: item.name,
+                devEst: item.devEst,
+                qcEst: item.qcEst
+            };
+        }
+    });
+    return result
+}
+function youtrackReq (type, URL, data, headers){
+    var youtrackBaseUrl = "https://maxymiser.myjetbrains.com/youtrack/rest/";
+    var request = {
+        type: type,
+        withCredentials: true,
+        url: youtrackBaseUrl + URL,
+        headers: {
+            'Accept':'application/json',
+            "Authorization": "Basic " + btoa("dmitriy.gorbachev@maxymiser.com:Salosila123")
+        },
+        data: data
+    };
+    return jQuery.ajax(request);
+}
 
 Template.workitems.helpers({
     "developers": function(){
@@ -19,23 +52,8 @@ Template.workitems.helpers({
     },
     "groupedLastWorkItems": function(){
         if(this.estimate){
-            var lastEstimate = this.estimate[this.estimate.length-1].items;
-            var result = {};
-            lastEstimate.forEach(function(item){
-                if(result[item.group]){
-                    result[item.group].name += ", " + item.name;
-                    result[item.group].devEst += item.devEst;
-                    result[item.group].qcEst += item.qcEst;
-                }
-                else{
-                    result[item.group] = {
-                        name: item.name,
-                        devEst: item.devEst,
-                        qcEst: item.qcEst
-                    };
-                }
-            });
-            return _.map(result, function(val,key){return {name: key, value: val}});
+            var result = summEstimate(this);
+            return _.map(result, function(val,key){return {group: key, value: val}});
         }
         else{
             return false;
@@ -85,6 +103,50 @@ Template.workitems.events({
     "click #setNewCampId": function(event, global){
         var newCampId = jQuery("#campaignId").val();
         Tests.update(global.data._id,{$set:{testId: newCampId}});
+    },
+    "click #createWorkItems": function(event, global){
+        var workitems = summEstimate(global.data);
+        var dev = global.data.dev;
+        var qc = global.data.qc;
+        var mainTicketId =  global.data.testId;
+        var promise;
+        Object.keys(workitems).forEach(function(key){
+            var item = workitems[key];
+            var createdItemId;
+            if(key == 1){
+                promise = youtrackReq("POST","issue/"+mainTicketId+"/execute?command=clone");
+                        promise.then(function() {
+                            console.log("Issue cloned");
+                            return youtrackReq("GET", "issue/?filter=created%3A+Today+created+by%3A+me+sort+by%3A+created+")
+                        })
+                        .then(function(data){
+                            console.log("data: ", data);
+                            console.log("this: ", this);
+                            createdItemId = data.issue[0].id;
+                            return youtrackReq("POST","issue/"+createdItemId+"/execute?command=Type Work Item Assignee "+dev.email+" Dev Estimate "+item.devEst+" QC Estimate "+item.qcEst+" Estimation "+(item.devEst+item.qcEst)+" Work Item State Backlog subtask of "+mainTicketId+" Developer "+dev.email+" QC Member "+qc.email+" ")
+                        })
+                        .then(function(){
+                            return youtrackReq("POST","issue/"+createdItemId+"?summary="+item.name+"&description="+item.name+"");
+                        });
+            }
+            else{
+                promise.then(function(){
+                    return youtrackReq("POST","issue/"+mainTicketId+"/execute?command=clone")
+                }).then(function() {
+                        console.log("Issue cloned");
+                        return youtrackReq("GET", "issue/?filter=created%3A+Today+created+by%3A+me+sort+by%3A+created+")
+                    })
+                    .then(function(data){
+                        console.log("data: ", data);
+                        console.log("this: ", this);
+                        createdItemId = data.issue[0].id;
+                        return youtrackReq("POST","issue/"+createdItemId+"/execute?command=Type Work Item Assignee "+dev.email+" Dev Estimate "+item.devEst+" QC Estimate "+item.qcEst+" Estimation "+(item.devEst+item.qcEst)+" Work Item State Backlog subtask of "+mainTicketId+" Developer "+dev.email+" QC Member "+qc.email+" ");
+                    })
+                    .then(function(){
+                        return youtrackReq("POST","issue/"+createdItemId+"?summary="+item.name+"&description="+item.name+"");
+                    });
+            }
+        });
     }
 
 });
