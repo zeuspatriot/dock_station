a09dfb6a49cdcc3b86f7bff3d23bd5080ee8f6ee
zeuspatriot <zeuspatriot@live.com>
2016-04-13 22:57:24 +0200

    - Login page rework * Roles and sectors moved to helpres

--- imports/ui/pages/login/login.helpers.js
+++ imports/ui/pages/login/login.helpers.js
@@ -1,7 +1,33 @@
 
 
 Template.loginPage.helpers({
-    "createAccount":function(){
+    createAccount: function(){
         return Session.get("isCreateAccount");
-    }
+    },
+    userProfile: function(){
+        var profile = {
+                name: "",
+                role: "",
+                email: "",
+                pass: "",
+                sector: ""
+            };
+        return profile;
+    },
+    sectors: [
+        "EMEA_Travel",
+        "EMEA_Retail",
+        "EMEA_Media&Telco",
+        "EMEA_Finance",
+        "EMEA_Gaming",
+        "US_Travel",
+        "US_Retail",
+        "US_Finance",
+        "US_Media"
+    ],
+    roles: [
+        "Dev",
+        "Qc",
+        "Ta"
+    ]
 });
