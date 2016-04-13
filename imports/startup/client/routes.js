407f8e4b5a5b6e20ebeaa70feb49855191f0f74d
zeuspatriot <zeuspatriot@live.com>
2016-04-13 21:53:01 +0200

    - Structure defined - Routers replaced

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,49 @@
+import {Meteor} from 'meteor/meteor';
+
+Router.route("/workitems/:_id",function(){
+    var currTest = Tests.findOne({_id:this.params._id});
+    this.render('workitems',{data:currTest});
+});
+Router.route("/users", {
+    template: "users",
+    name: "users"
+});
+Router.route("/",{
+    template: "clientList",
+    name: "tests"
+});
+
+Router.route("/login/",{
+    template:"loginPage",
+    name:"login"
+});
+Router.route("/clients",{
+    template: "clientList",
+    name: "clientList"
+});
+Router.route("/clients/:client",function(){
+    var userSector = "";
+    if(Meteor.user()) userSector = Meteor.user().profile.sector;
+    var clientTests = Tests.find({client:this.params.client,sector: userSector});
+    var result = {
+        clientName: this.params.client,
+        clientTests: clientTests
+    };
+
+    this.render('tests',{data:result});
+});
+
+Router.onBeforeAction(function () {
+    if (!Meteor.user() && !Meteor.loggingIn()) {
+        this.redirect('/login');
+    } else {
+        // required by Iron to process the route handler
+        var userSector = "";
+        if(Meteor.user()) userSector = Meteor.user().profile.sector;
+        Meteor.subscribe("tests", userSector);
+        Meteor.subscribe('users',userSector);
+        this.next();
+    }
+}, {
+    except: ['login']
+});
---
+++ server/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by zeusp_000 on 13-Apr-16.
+ */
