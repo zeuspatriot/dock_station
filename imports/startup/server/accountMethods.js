5c75a50b8497d061c3ebd74f0a8c1aec46462aea
zeuspatriot <zeuspatriot@live.com>
2016-04-20 22:05:00 +0200

    Profile page implemented

--- imports/startup/server/accountMethods.js
+++ imports/startup/server/accountMethods.js
@@ -2,8 +2,9 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 Meteor.methods({
-    changeUserPassword: function(oldPassword, newPassword){
-        return Accounts.changePassword(oldPassword, newPassword);
+    changeUserPassword: function(userId, newPassword){
+        return Meteor.users.update(userId, {$set:{"profile.pass":newPassword}});
+        //return Accounts.changePassword(oldPassword, newPassword);
     },
     changeUserUsername: function(userId, newUsername){
         if(userId === Meteor.user()._id){
@@ -12,5 +13,8 @@ Meteor.methods({
         else{
             throw new Error("User can change only own data. (Current user: ", Meteor.user()._id);
         }
+    },
+    changeUserData: function(userId, data){
+        return Meteor.users.update(userId, data);
     }
 });
--- imports/ui/pages/profile/profile.events.js
+++ imports/ui/pages/profile/profile.events.js
@@ -20,23 +20,21 @@ Template.profilePage.events({
     },
     "click #submit": function(){
         var currUserId = Meteor.user()._id;
-        var oldPass = jQuery("#oldPassword").val();
-        var newPass = jQuery("#newPassword").val();
+
         var newLogin = jQuery("#login").val();
         var newName = jQuery("#name").val();
         var newSector = jQuery("#userSector").val();
         var newRole = jQuery("#userRole").val();
-
-        Meteor.users.update(currUserId,
-            {$set:
-                {
-                    "profile.name":newName,
-                    "profile.role":newRole,
-                    "profile.sector": newSector,
-                    "profile.email": newLogin
-                }
+        var data = {$set:
+            {
+                "profile.name":newName,
+                "profile.role":newRole,
+                "profile.sector": newSector,
+                "profile.email": newLogin
             }
-        );
+        };
+
+        Meteor.call("changeUserData", currUserId, data);
 
         youtrackApi.loginToYoutrack(newLogin, Meteor.user().profile.pass).done(function(data){
             Meteor.call("changeUserUsername",currUserId, newLogin, function(err, res){
@@ -49,9 +47,30 @@ Template.profilePage.events({
 
 
 
-        if(oldPass && newPass){
-            Meteor.call("changeUSerPassword", oldPass, newPass);
-            //Accounts.changePassword(oldPass, newPass);
+
+    },
+    'submit #submitChangePassword': function(event){
+        event.preventDefault();
+        var oldPass = event.target.oldPassword.value;
+        var newPass = event.target.newPassword.value;
+        console.log(oldPass,newPass);
+
+        if(oldPass != newPass){
+            Accounts.changePassword(oldPass, newPass, function(err, res){
+                if (err) {
+                    console.error(err);
+                    sAlert.error(err.message);
+                }
+                else{
+                    sAlert.success("Password successfully changed");
+                    event.target.oldPassword.value = "";
+                    event.target.newPassword.value = "";
+                    Meteor.call("changeUserPassword", Meteor.user()._id, newPass);
+                }
+            });
+        }
+        else{
+            sAlert.error("New Password must not be same as old password");
         }
     }
 });
